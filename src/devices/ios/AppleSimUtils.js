241153f1fc786ff5594e96ea7d5133d2f1683c3e
hcondron <condronharry@gmail.com>
2019-11-01 12:44:14 +0000

    Change apple simutils to stop blacklisting of expo url

--- src/devices/ios/AppleSimUtils.js
+++ src/devices/ios/AppleSimUtils.js
@@ -266,7 +266,7 @@ class AppleSimUtils {
   }
 
   _joinLaunchArgs(launchArgs) {
-    return _.map(launchArgs, (v, k) => `-${k} "${v}"`).join(' ').trim();
+     return _.map(launchArgs, (v, k) => `-${k} ${v}`).join(' ').trim();
   }
 
   async _launchMagically(frameworkPath, udid, bundleId, launchArgs, languageAndLocale) {
